6784bd17d092e0cd3b8ac776d45dce66bc7723c4
Joshua-Noakes1 <joshua.noakes1@icloud.com>
2020-10-21 20:28:36 +0100

    used mv not move in the move script

--- SETUP_QDAT.bat
+++ SETUP_QDAT.bat
@@ -5,8 +5,8 @@ cls
 
 echo Configuring...
 mkdir C:\Azgard
-mv dl_ess.tx C:\Azgard\dl_ess.zip
+move dl_ess.tx C:\Azgard\dl_ess.zip
 powershell -command "Expand-Archive 'C:\Azgard\dl_ess.zip' 'C:\Azgard\'"
-mv QDAT.tx QDAT.bat
+move QDAT.tx QDAT.bat
 start /d "C:\Azgard" QDAT.bat
 exit
