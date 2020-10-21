4c2370ead4e16fbe1b6a4fbef34155fc03b0cfe4
Joshua Noakes <joshua.noakes1@icloud.com>
2020-10-21 10:58:22 +0100

    Update QADT.bat

--- QADT.bat
+++ QADT.bat
@@ -2,8 +2,13 @@
 title QADT - Quick And Dirty Tool
 color f5
 
-mkdir C:\QADT
-cd C:\QADT
+mkdir C:\Azgard
+cd C:\Azgard
+echo Downloading
+powershell -command "iwr -outf dl_ess.zip https://cdn.joshuanoakes.co.uk/cdn/dl/dl_ess.zip"
+echo Download complete 
+powershell -command "Expand-Archive 'dl_ess.zip' 'dl_ess\'"
+del dl_ess.zip
 
 :Menu
 cls
@@ -11,24 +16,28 @@ echo ------------------------------
 echo QADT - Quick And Dirty Tool
 echo ------------------------------
 echo.
-echo 1 - Download Essentials
+echo 1 - Software
 echo 2 - Setup Workspace 
 echo 99 - Exit
 echo.
 set /P MENU=Type options: 
-if %MENU%==1 goto dless
+if %MENU%==1 goto program
 if %MENU%==2 goto setwork
 if %MENU%==99 exit
 goto Menu
 
-:dless
+:program
 cls
-echo Downloading
-powershell -command "iwr -outf dl_ess.zip https://cdn.joshuanoakes.co.uk/cdn/dl/dl_ess.zip"
-echo Download complete 
-powershell -command "Expand-Archive 'dl_ess.zip' 'dl_ess\'"
-del dl_ess.zip
-goto Menu
+echo ------------------------------
+echo QADT - Quick And Dirty Tool
+echo ------------------------------
+echo.
+echo 1 - Anydesk
+echo 99 - Exit
+echo.
+set /P PROGRAM=Type options: 
+if %PROGRAM%==1 start dl-ess/software/anydesk.exe
+if %PROGRAM%==99 exit
 
 :setwork
 cls
